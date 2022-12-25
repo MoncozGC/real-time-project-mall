142703b283d2b33e7e2fb1b5e365839c19b0fa4c
MoncozGC <jadepengc@163.com>
2022-12-25 11:01:22 +0800

    feat: 修改主程序名称

--- dw-chart/src/main/java/com/demo/DwChartApplication.java
+++ dw-chart/src/main/java/com/demo/DwChartApplication.java
@@ -7,9 +7,9 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
-public class DemoApplication {
+public class DwChartApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(DwChartApplication.class, args);
     }
 }
